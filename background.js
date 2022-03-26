8df2b71c0d7c4a89e9b2480238d902f74b8087dd
Todd Rimes <todd.rimes@nbcuni.com>
2022-03-26 10:46:03 -0400

    more manifest permissions baloney resolved!

--- background.js
+++ background.js
@@ -2,6 +2,22 @@ console.log("Hi from background Script file")
 
 let jiraObject = {};
 
+// background.js
+chrome.action.onClicked.addListener((tab) => {
+    chrome.scripting.executeScript({
+        target: {tabId: tab.id},
+        files: ['content.js']
+    }, fireItUp());
+});
+
+function fireItUp() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {greeting: "getMoving"}, function(response) {
+            console.log(response);
+        });
+    });
+}
+
 chrome.runtime.onMessage.addListener( function (request, sender, sendResponse) {
     console.log("Got message from content Script: ", request);
     jiraObject = request;
@@ -16,34 +32,37 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     if(changeInfo.status == 'complete') {
         switch (true) {
             case (tab.url.indexOf('https://wiki.inbcu.com/display/ADSYS/VAM+User+Stories') != -1):
+                let x = 1;
                 chrome.scripting.executeScript({
-                    target: {tabId: tab.id},
-                    function: myFunction
+                    target: {tabId: tabId, allFrames: true},
+                    func: myFunction
                 });
                 break;
             case (tab.url.indexOf('https://wiki.inbcu.com/pages/createpage.action?spaceKey=ADSYS&fromPageId=') != -1):
+                let y = 1;
                 chrome.scripting.executeScript({
-                    target: {tabId: tab.id},
-                    function: myFunction2
+                    target: {tabId: tabId, allFrames: true},
+                    func: myFunction2
                 });
                 break;
             case (tab.url.indexOf('https://wiki.inbcu.com/display/ADSYS/') != -1):
+                let z = 1;
                 let wikiURL = tab.url;
                 chrome.scripting.executeScript({
-                    target: {tabId: tab.id},
-                    function: myFunction3(wikiURL)
+                    target: {tabId: tabId, allFrames: true},
+                    func: myFunction3
                 });
                 break;
         }
     }
 });
 
-function myFunction() {
+async function myFunction() {
     const createPageButton = document.getElementById('quick-create-page-button');
     createPageButton.click();
 }
 
-function myFunction2() {
+async function myFunction2() {
     //paste jirtitle into #content-title
     const wikiTitle = document.getElementById('content-title');
     wikiTitle.innerText = jiraObject.jiraTitleText;
@@ -53,9 +72,13 @@ function myFunction2() {
     // click the button with id #rte-button-publish
     const publishButton = document.getElementById('rte-button-publish');
     publishButton.click();
+    
+    // then wait to get the URL
+    driver.find_element(By.ID, "shareContentLink").click()
+    
 }
 
-function myFunction3(theURL) {
+async function myFunction3(theURL,mTabId) {
     // send theURL into the original Jira description
     chrome.tabs.sendMessage(jiraObject.jiraTabId, {wikiURL: theURL}, function(response) {
         console.log(response.farewell);
--- content.js
+++ content.js
@@ -3,3 +3,36 @@ console.log("Hi from content script");
 chrome.runtime.sendMessage({ data: document.title }, function (response) {
     console.log(response);
 });
+
+async function handleIconClick(tab) {
+    // const current = e.target.parentElement;
+    console.log("copy-to-wiki button pressed");
+    alert("hello!");
+    chrome.scripting.executeScript({
+        target: { tabId:tab.id },
+        function: copyJiraToWiki(tab.id)
+    });
+}
+
+function copyJiraToWiki(mTabId) {
+    // document.body.innerHTML = jiraDescriptionHtml;
+    let jiraMessage = {
+        ":jiraTitleText" : document.querySelector('#summary-val').innerText,
+        "jiraDescriptionHtml": document.querySelector('#description-val').innerHTML,
+        "jiraTabId": mTabId
+    }
+    chrome.runtime.sendMessage(jiraMessage, function(response) {
+        console.log(response);
+    });
+}
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+      let wikiURL = request;
+      document.querySelector('#description-val').innerHTML = wikiURL;
+      console.log(sender.tab ?
+        "from a content script:" + sender.tab.url :
+        "from the extension");
+      sendResponse({farewell: "goodbye"});
+  }
+);
